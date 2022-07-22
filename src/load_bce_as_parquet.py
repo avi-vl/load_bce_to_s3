b5e2f2394d434dbf5a847c56d42b27b1d5b60981
Derry Holding <derry.holding@mediengruppe-rtl-extern.de>
2022-07-22 09:07:00 +0200

    Restructured and created generator

--- src/load_bce_as_parquet.py
+++ src/load_bce_as_parquet.py
@@ -2,43 +2,41 @@ import time
 
 import pandas as pd
 
-from bce_connection import bce_connection
+from bce_connection import BceConnection
 from constants import STDOUT, CHUNKSIZE, table_name
 from sql_queries import sql_query
-from utils import export_parquet_to_s3
+from utils import export_parquet_to_s3, retrieve_chunks
 
 
+class BceDateToParquet:
+    def main(self):
+        """Connect to BCE, retrieve data via an SQL query, and upload into S3 as parquet files
 
-def main():
-    engine = bce_connection()
+        :return: None
+        """
+        # Connect to BCE
+        bce = BceConnection()
+        engine = bce.bce_open_connection()
 
-    create_parquet_file(
-        sql_query,
-        engine,
-        table_name
-    )
+        # Store the results of the SQL query as dataframe
+        s1 = time.process_time()
+        chunks = retrieve_chunks(sql_query, engine)
 
+        # Since chunksize is specified a concat is necessary
+        df = pd.concat(list(chunks))
+        STDOUT.write(f"Building iterator of chunksize {CHUNKSIZE} took (s): "
+                     f"{time.process_time() - s1}\n")
 
-def create_parquet_file(sql_query, engine, report_id):
-    # store the results of the SQL query as dataframe
-    start_time_iterator = time.process_time()
+        s2 = time.process_time()
+        export_parquet_to_s3(df=df,
+                             table_name=table_name)
+        STDOUT.write(f'Exporting of parquet file (s): '
+                     f'{time.process_time() - s2}\n')
 
-    # since chunksize is specified a concat is necessary
-
-    chunks = []
-    for chunk in pd.read_sql_query(sql_query, engine, coerce_float=True, chunksize=CHUNKSIZE):
-        chunks.append(chunk)
-    df = pd.concat(list(chunks))
-
-    STDOUT.write(f"Building iterator of chunksize {CHUNKSIZE} took (s): "
-                 f"{time.process_time() - start_time_iterator}\n")
-
-    start_time_parquet = time.process_time()
-    export_parquet_to_s3(df=df,
-                         table_name=report_id)
-    STDOUT.write(f'Parquet file built in (s): '
-                 f'{time.process_time() - start_time_parquet}\n')
+        # Disconnect from BCE
+        bce.bce_close_connection(engine)
 
 
 if __name__ == "__main__":
-    main()
+    bce_data = BceDateToParquet()
+    bce_data.main()
--- src/utils.py
+++ src/utils.py
@@ -2,47 +2,22 @@ from datetime import datetime
 
 import awswrangler as wr
 import pandas as pd
-import pyarrow as pa
 
-from constants import STDOUT, STDERR, S3_STAGING_BUCKET, S3_STAGING_PREFIX
+from constants import STDERR, S3_STAGING_BUCKET, S3_STAGING_PREFIX, CHUNKSIZE
 
 
-def convert_types(df: pd.DataFrame, df_schema: dict) -> pd.DataFrame:
-    """ Cast data types on the dataframe columns and overwrite some values.
-    Problematic values are 'nan' and 'inf' within the numeric columns. Columns,
-    which could contain 'inf', should be casted to type 'float'. For integer
-    types 'nan' will be casted to 0.
-    Finally, in the string columns 'None' is replaced with '' as the export
-    from pandas to PARQUET would automatically cast NoneType to the 'None'
-    string.
-    :param df: Original dataframe to be casted.
-    :param df_schema: Schema to be applied. See 'tables.py' for the simplified
-        schema definition.
-    :return: Casted and preprocessed dataframe.
+def retrieve_chunks(sql_query, engine):
     """
-    for col in df_schema.get('int', {}).get('columns', []):
-        df[col] = df[col].apply(lambda x: '0' if str(x) == 'nan' else x)
 
-    for col in df_schema.get('bool', {}).get('columns', []):
-        if col in df.columns and str(df[col].dtype) == 'object':
-            df[col] = df[col].astype(dtype=str)
-            mask = df[col] != 'True'
-            df.loc[mask, col] = None
+    :param sql_query:
+    :param engine:
+    :return:
+    """
+    def chunk_generator():
+        for chunk in pd.read_sql_query(sql_query, engine, coerce_float=True, chunksize=CHUNKSIZE):
+            yield chunk
 
-    for conversion in df_schema:
-        tp = df_schema[conversion]['type']
-        for column in df_schema[conversion]['columns']:
-            try:
-                df[column] = df[column].astype(dtype=tp)
-                if conversion == 'str':
-                    mask = df[column].isin(['None', 'nan'])
-                    df.loc[mask, column] = None
-            except Exception as e:
-                msg = 'Failed to convert column '
-                msg += f'{column} to type {conversion}!'
-                print(msg)
-                raise e
-    return df
+    return list(chunk_generator())
 
 
 def export_parquet_to_s3(
@@ -51,26 +26,21 @@ def export_parquet_to_s3(
         prefix: str = S3_STAGING_PREFIX,
         table_name: str = None,
 ) -> None:
-    databases = wr.catalog.databases()
+    """
 
-    if "bce_glue" not in databases.values:
-        wr.catalog.create_database("bce_glue")
-        STDOUT.write(wr.catalog.databases())
-    else:
-        STDERR.write("Database bce_glue already exists\n")
-
-    a = wr.catalog.tables(database="bce_glue")
-
+    :param df:
+    :param bucket:
+    :param prefix:
+    :param table_name:
+    :return:
+    """
     date = datetime.now().strftime("%Y/%m/%d")
-    p = f"s3://{bucket}/{prefix}{table_name}/{date}"
 
     # writing df to Data Lake (S3 + Parquet + Glue Catalog)
     response = wr.s3.to_parquet(
         df=df,
         path=f"s3://{bucket}/{prefix}{table_name}/{date}",
         dataset=True,
-        database="bce_glue",
-        table=table_name,
         mode="overwrite"
     )
 
